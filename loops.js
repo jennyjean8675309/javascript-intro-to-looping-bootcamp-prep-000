72926b1e09629b0bd07b8fbc2973e1ed94ab9e19
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-07 15:56:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array){
   for (var i = 0; i < 25; i++) {
   if (i === 1){
-    return array + "I am 1 strange loop."; 
+    return array.push("I am 1 strange loop."); 
     } else {
-        return array + `I am ${i} strange loops.`;
+        return array.push(`I am ${i} strange loops.`);
   }
 }
 }
